ef5d4bc44111b436ed12d09cc8e725c171832c54
Yan Rodrigues <ccyan05@alu.ufc.br>
2022-02-06 17:28:24 -0300

    Red black def

---
+++ src/lib/red_tree.h
@@ -0,0 +1,18 @@
+#ifndef RTREE
+#define RTREE
+
+typedef struct node
+{
+    struct node *left;
+    struct node *right;
+    struct node *parent;
+    int key;
+    char color;
+} Node;
+
+Node *create_node(int key);
+Node *create_tnil();
+void pre_order(Node *root, Node *tnil);
+void insert_tree(Node **root, Node *z, Node *tnil);
+
+#endif
