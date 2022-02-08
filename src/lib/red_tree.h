e92f9311a0d8f702c8423aa432795ab96f7e39b5
Yan Rodrigues <ccyan05@alu.ufc.br>
2022-02-08 11:32:16 -0300

    Adding find def and remove def

--- src/lib/red_tree.h
+++ src/lib/red_tree.h
@@ -12,8 +12,12 @@ typedef struct node
 
 Node *create_node(int key);
 Node *create_tnil();
-void pre_order(Node *root, Node *tnil);
+
 int tree_size(Node *root, Node *tnil);
+void pre_order(Node *root, Node *tnil);
+Node *find(Node **root, int key, Node *tnil);
+
 void insert_tree(Node **root, Node *z, Node *tnil);
+void remove_tree(Node **root, Node *z, Node *tnil);
 
 #endif
